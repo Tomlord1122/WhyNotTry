ad33829313a1329416624528b0332c55f141ec87
tomlord <aa123593465@gmail.com>
2024-06-25 17:15:49 +0800

    Initial Commit

---
+++ WhyNotTry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhyNotTry
+//
+//  Created by 領主湯姆 on 2024/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
